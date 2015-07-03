41a01cc8889bf0339619ac9c7b45cc97aa1b7675
Kangear <kangear@163.com>
2015-07-03 16:14:26 +0800

    实现打开串口操作

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,45 +1,74 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+#include <QImageReader>
+#include <QFile>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    init();
+}
 
-    font = QFont( "Arial", 8);
+void MainWindow::init()
+{
+    // 字体大小
+    font = QFont( "Arial", 10);
 
-    // 状态栏
-    statusLabel = new QLabel;
-    statusLabel->setMinimumSize(20, 14); // 设置标签最小大小
-    statusLabel->setText("UDP通信停止");
-    statusLabel->setAlignment(Qt::AlignHCenter);
-    statusLabel->setFont(font);
+    // 状态
+    isOn = false;
+    ui->led_label->setPixmap(QPixmap(":/led/off"));
 
-
-    // 接收数量
-    receiveLabel = new QLabel;
-    receiveLabel->setMinimumSize(20, 14); // 设置标签最小大小
-    receiveLabel->setText("UDP通信停止");
-    receiveLabel->setAlignment(Qt::AlignHCenter);
-    receiveLabel->setFont(font);
-
-
-
+    // 网址
+    netAddrLabel = new QLabel;
+    netAddrLabel->setMinimumSize(20, 14); // 设置标签最小大小
+    netAddrLabel->setText("www.daxia.com");
+    netAddrLabel->setAlignment(Qt::AlignHCenter);
+    netAddrLabel->setFont(font);
 
     // 发送数量
     sendLabel = new QLabel;
-    sendLabel->setMinimumSize(20, 14); // 设置标签最小大小
-    sendLabel->setText("UDP通信停止");
-    sendLabel->setAlignment(Qt::AlignHCenter);
+    sendLabel->setMinimumSize(100, 14); // 设置标签最小大小
+    sendLabel->setText("S:0");
+    sendLabel->setAlignment(Qt::AlignLeft);
     sendLabel->setFont(font);
 
-    ui->statusBar->addWidget(statusLabel);
+    // 接收数量
+    receiveLabel = new QLabel;
+    receiveLabel->setMinimumSize(100, 14); // 设置标签最小大小
+    receiveLabel->setText("R:0");
+    receiveLabel->setAlignment(Qt::AlignLeft);
+    receiveLabel->setFont(font);
+
+    // 状态栏
+    statusLabel = new QLabel;
+    statusLabel->setMinimumSize(100, 14); // 设置标签最小大小
+    statusLabel->setText("ttyUSB0 已关闭 115200bps,8,1 无检验 无流控");
+    statusLabel->setAlignment(Qt::AlignHCenter);
+    statusLabel->setFont(font);
+
+    ui->statusBar->addWidget(netAddrLabel);
     ui->statusBar->addWidget(receiveLabel);
     ui->statusBar->addWidget(sendLabel);
+    ui->statusBar->addWidget(statusLabel);
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_openserial_pushButton_pressed()
+{
+    if(isOn) {
+        isOn = false;
+        ui->led_label->setPixmap(QPixmap(":/led/off"));
+    } else {
+        isOn = true;
+        ui->led_label->setPixmap(QPixmap(":/led/on"));
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,15 +17,22 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_openserial_pushButton_pressed();
+
 private:
     Ui::MainWindow *ui;
-    /** 状态标签 */
+    /** 网址 */
     QLabel *statusLabel;
+    /** 状态标签 */
+    QLabel *netAddrLabel;
     QFont font;
     /** 总接收数量显示标签 */
     QLabel *receiveLabel;
     /** 总发送数量显示标签 */
     QLabel *sendLabel;
+    bool isOn;
+    void init();
 };
 
 #endif // MAINWINDOW_H
